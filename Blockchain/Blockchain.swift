7c56c41b30ab04af77ebaf410db2f0370fbe4400
Edy <elshad040@gmail.com>
2019-03-09 18:19:21 +0400

    Blockchain

---
+++ Blockchain/Blockchain.swift
@@ -0,0 +1,32 @@
+//
+//  Blockchain.swift
+//  Blockchain
+//
+//  Created by Edy on 3/7/19.
+//  Copyright © 2019 App Millers. All rights reserved.
+//
+
+import UIKit
+
+class Blockchain {
+    //logic for the Blockchain here
+    var chain = [Block]()
+    
+    func createInitialBlock(data:String) {
+        let genesisBlock = Block()
+        genesisBlock.hash = genesisBlock.generateHash()
+        genesisBlock.data = data
+        genesisBlock.previousHash = "0000"
+        genesisBlock.index = 0
+        chain.append(genesisBlock)
+    }
+    
+    func createBlock(data:String) {
+        let newBlock = Block()
+        newBlock.hash = newBlock.generateHash()
+        newBlock.data = data
+        newBlock.previousHash = chain[chain.count-1].hash
+        newBlock.index = chain.count
+        chain.append(newBlock)
+    }
+}
--- Blockchain/ViewController.swift
+++ Blockchain/ViewController.swift
@@ -3,18 +3,42 @@
 //  Blockchain
 //
 //  Created by Edy on 3/7/19.
-//  Copyright © 2019 Edy. All rights reserved.
+//  Copyright © 2019 App Millers. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var greenAmount: UITextField!
+    @IBOutlet weak var redAmount: UITextField!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func redMine(_ sender: Any) {
+    }
+    
+    @IBAction func greenMine(_ sender: Any) {
+    }
+    
+    @IBAction func redSend(_ sender: Any) {
+    }
+    
+    @IBAction func greenSend(_ sender: Any) {
+    }
 
 
+}
+
+extension ViewController {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
